ea8ba91755c9a72927f4a6d1417ed28b4746f109
Govindaiahanilkumar <118826336+Govindaiahanilkumar@users.noreply.github.com>
2022-11-24 17:32:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
+streamlit.caption('Omega3 and blueberry Oatmeal')
+streamlit.caption('Kale, Spinach and rocket smothie')
