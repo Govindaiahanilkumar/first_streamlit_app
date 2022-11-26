0a25348ee474529288e46d082e2baf6a01c14e48
Govindaiahanilkumar <118826336+Govindaiahanilkumar@users.noreply.github.com>
2022-11-26 11:23:05 +0100

    Update streamlit_app.py
    import pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ streamlit.text('🥗Kale, Spinach and Rocket smothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
